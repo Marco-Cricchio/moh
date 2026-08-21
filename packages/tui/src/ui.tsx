c2b2e92942a532a9672b817f76a2a8b54f3f3711
Marco Cricchio <mcricchio@gmail.com.com>
2026-08-21 15:11:39 +0200

    fix(tui): center settings and other dialogs on screen

--- packages/tui/src/ui.tsx
+++ packages/tui/src/ui.tsx
@@ -70,7 +70,7 @@ export function Dialog({
 }) {
   const theme = useTheme();
   return (
-    <Box justifyContent="center" flexDirection="column">
+    <Box width="100%" height="100%" alignItems="center" justifyContent="center" flexDirection="column">
       <Box
         borderStyle="round"
         borderColor={color}
